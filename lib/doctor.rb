ae4951d442ea9631e944257f4aedb621d5c7bd1b
Jason Mei <jason.mei403@gmail.com>
2018-06-27 20:26:38 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -13,7 +13,7 @@ class Doctor
   end
 
   def new_appointment(patient, date)
-    appointment = Appointment.new(date, patient, self)
+    appointment = Appointment.new(patient, self, date)
     appointment
   end
 
