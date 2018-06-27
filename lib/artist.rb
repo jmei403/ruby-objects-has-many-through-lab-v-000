de0dcf3608e77d01d7f60e1b71de202489d04b5c
Jason Mei <jason.mei403@gmail.com>
2018-06-27 19:51:18 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,25 @@
+class Artist
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(name, genre)
+
+  end
+
+  def songs
+  end
+
+  def genres
+  end
+  
+end
