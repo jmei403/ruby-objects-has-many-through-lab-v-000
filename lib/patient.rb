00ade2d9a6bfe2fa5cdd6907c99ab81cb925cba5
Jason Mei <jason.mei403@gmail.com>
2018-06-27 20:11:57 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -4,9 +4,13 @@ class Patient
   def initialize(name)
     @name = name
     @@all << self
-  end 
+  end
 
   def self.all
     @@all
   end
+
+  def new_appointment(doctor, date)
+    appointment = Appointment.new(date, self, doctor)
+    appointment
 end
